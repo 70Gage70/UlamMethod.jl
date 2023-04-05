e9b2c160f892fbb7e672c35e1da5edf4a553e9a1
Gage <gage.bonner@gmail.com>
2023-04-05 17:01:29 -0400

    types in module etc.

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,11 @@
-const global_bin_types = ["reg", "hex", "vor"]
-const global_poly_types = ["reg", "hex", "vor", "unk"]
+module UlamTypes
+
+export 
+    AbstractInPolygonCompatible,
+    UlamPolygon,
+    PolyTable,
+    UlamCovering
+
 
 abstract type AbstractInPolygonCompatible end
 
@@ -17,7 +23,7 @@ struct UlamPolygon <: AbstractInPolygonCompatible
 
         @assert size(nodes, 1) > 0 
         @assert size(nodes, 2) == 2 
-        @assert polys_type in global_poly_types
+        @assert polys_type in ["reg", "hex", "vor", "unk"]
 
         n_nodes = size(nodes, 1)
 
@@ -107,77 +113,79 @@ struct UlamCovering
 
 end
 
-include("binner-square.jl")
-n_polys_test = 100
-polys_test, polys_centers_test = square_binner(n_polys_test, [0, 1, 0, 1])
-include("ulam-nirvana.jl")
-iptest = inpoly_preprocess(polys_test)
-uptest = [UlamPolygon(iptest["nodes"][i:i+3,:]) for i=1:4:4*n_polys_test]
-uctest = UlamCovering(uptest)
-pttest = PolyTable(uctest.polys)
+end # module
+
+# include("binner-square.jl")
+# n_polys_test = 100
+# polys_test, polys_centers_test = square_binner(n_polys_test, [0, 1, 0, 1])
+# include("ulam-nirvana.jl")
+# iptest = inpoly_preprocess(polys_test)
+# uptest = [UlamPolygon(iptest["nodes"][i:i+3,:]) for i=1:4:4*n_polys_test]
+# uctest = UlamCovering(uptest)
+# pttest = PolyTable(uctest.polys)
 
 
 ##############################################################################################################################
 
 # To define an Ulam method, need corners, x0y0xTyT data, binning type and number, stochasticization type 
 
-struct UlamBoundary 
-    xmin::Float64
-    xmax::Float64
-    ymin::Float64
-    ymax::Float64
+# struct UlamBoundary 
+#     xmin::Float64
+#     xmax::Float64
+#     ymin::Float64
+#     ymax::Float64
 
-    function UlamBoundary(;xmin::R1, xmax::R2, ymin::R3, ymax::R4) where {R1 <: Real, R2 <: Real, R3 <: Real, R4 <: Real}
-        @assert xmin < xmax
-        @assert ymin < ymax
-        new(xmin, xmax, ymin, ymax)
-    end
-end  
+#     function UlamBoundary(;xmin::R1, xmax::R2, ymin::R3, ymax::R4) where {R1 <: Real, R2 <: Real, R3 <: Real, R4 <: Real}
+#         @assert xmin < xmax
+#         @assert ymin < ymax
+#         new(xmin, xmax, ymin, ymax)
+#     end
+# end  
 
-struct UlamTrajectories 
-    x0::Vector{Float64}
-    xT::Vector{Float64}
-    y0::Vector{Float64}
-    yT::Vector{Float64}
+# struct UlamTrajectories 
+#     x0::Vector{Float64}
+#     xT::Vector{Float64}
+#     y0::Vector{Float64}
+#     yT::Vector{Float64}
 
-    function UlamTrajectories(;x0::Vector{R1}, y0::Vector{R2}, xT::Vector{R3}, yT::Vector{R4}) where {R1 <: Real, R2 <: Real, R3 <: Real, R4 <: Real}
-        @assert length(x0) == length(y0) == length(xT) == length(yT) > 0
-        new(x0, xT, y0, yT)
-    end
-end  
+#     function UlamTrajectories(;x0::Vector{R1}, y0::Vector{R2}, xT::Vector{R3}, yT::Vector{R4}) where {R1 <: Real, R2 <: Real, R3 <: Real, R4 <: Real}
+#         @assert length(x0) == length(y0) == length(xT) == length(yT) > 0
+#         new(x0, xT, y0, yT)
+#     end
+# end  
 
-struct UlamBins 
-    bin_type::String
-    bin_number::Integer
+# struct UlamBins 
+#     bin_type::String
+#     bin_number::Integer
 
-    function UlamBins(;bin_type::String, bin_number::Integer)
-        @assert bin_type in global_bin_types
-        @assert bin_number > 0
-        new(bin_type, bin_number)
-    end
-end  
+#     function UlamBins(;bin_type::String, bin_number::Integer)
+#         @assert bin_type in ["reg", "hex", "vor"]
+#         @assert bin_number > 0
+#         new(bin_type, bin_number)
+#     end
+# end  
 
-struct UlamStocData
-    stoc_type::String
-    bin_number::Integer
+# struct UlamStocData
+#     stoc_type::String
+#     bin_number::Integer
 
-    function UlamBins(;bin_type::String, bin_number::Integer)
-        @assert bin_type in global_bin_types
-        @assert bin_number > 0
-        new(bin_type, bin_number)
-    end
-end  
+#     function UlamBins(;bin_type::String, bin_number::Integer)
+#         @assert bin_type in ["reg", "hex", "vor"]
+#         @assert bin_number > 0
+#         new(bin_type, bin_number)
+#     end
+# end  
 
 
 
-struct UlamProblem
-    corners::Vector{T} where {T<:Real}
+# struct UlamProblem
+#     corners::Vector{T} where {T<:Real}
 
-    function UlamProblem(corners::Vector{T}) where {T<:Real}
-        if length(corners) != 4
-            throw(ArgumentError("Vector must have length 4"))
-        else
-            new(corners)
-        end
-    end
-end
+#     function UlamProblem(corners::Vector{T}) where {T<:Real}
+#         if length(corners) != 4
+#             throw(ArgumentError("Vector must have length 4"))
+#         else
+#             new(corners)
+#         end
+#     end
+# end

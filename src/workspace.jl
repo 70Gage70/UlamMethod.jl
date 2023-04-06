1a40d9d555265e5f8a9c892fbf0a27bb1b304559
Gage <gage.bonner@gmail.com>
2023-04-06 14:18:50 -0400

    up to scc good

--- src/workspace.jl
+++ src/workspace.jl
@@ -8,65 +8,42 @@ traj = UlamTrajectories("x0x5-NA-undrogued.mat");
 domain = UlamDomain(-100, 15, -9, 39, poly_number = 500);
 polys = square_binner(traj, domain);
 
-# function get_scc_inds_old(P_closed)
-#     Psize = length(P_closed[1,:])
-#     P_open_size = Psize - 1 # exclude nirvana when calculating scc, assumes one nirvana state in last entry
-    
-#     Padj = Matrix{Int64}(undef, P_open_size, P_open_size) # the adjacency matrix defined by P
-#     for i = 1:P_open_size
-#         for j = 1:P_open_size
-#             if P_closed[i, j] == 0.0
-#                 Padj[i,j] = 0 
-#             else
-#                 Padj[i,j] = 1
-#             end
-#         end
-#     end
-    
-#     Pgraph = SimpleDiGraph(Padj)
-#     scc = sort(strongly_connected_components(Pgraph), by = length) 
-#     largest_component = sort(scc[end])
-#     push!(largest_component, Psize) # put nirvana back at the end, should stay sorted
-
-#     return largest_component
-# end
-
 #########################################################################################################
 #########################################################################################################
 #########################################################################################################
 
 ### Assign polygon indices to trajectory points
 
-# find the polygon indices of initial trajectory points 
+# find the polygon indices of initial and final trajectory points 
 ip0 = inpoly([traj.x0 ;; traj.y0], PolyTable(polys));
+ipT = inpoly([traj.xT ;; traj.yT], PolyTable(polys));
 
 # discard polygons that don't contain data
-contains_data = [i for i in keys(ip0.contains)]
-polys = polys[contains_data]
+polys = polys[[i for i in keys(ip0.contains)]]
 n_polys = length(polys)
-
-# find the polygon indices of final trajectory points
-ipT = inpoly([traj.xT ;; traj.yT], PolyTable(polys));
 
 ### Construct P_closed
 
 P_closed = zeros(n_polys + 1, n_polys + 1) # one extra for nirvana
 
-for i = 1:length(ip0.inds)
-    i0, iT = ip0.inds[i], ipT.inds[i]
-    if i0 != 0 # point starts in domain
-        i0 = ip0.contains[i0] # i0 is still with respect to the uncleaned polygons, so we just fix that with the contains Dict
-        if iT != 0 # point ends in domain
-            P_closed[i0, iT] = P_closed[i0, iT] + 1
-        elseif iT == 0 # point ends outside domain
-            P_closed[i0, n_polys + 1] = P_closed[i0, n_polys + 1] + 1
-        end
-    elseif i0 == 0 # point starts outside domain
-        if iT != 0 # point ends in domain (reinjection)
-            P_closed[n_polys + 1, iT] = P_closed[n_polys + 1, iT] + 1
-        end
+for i = 1:length(inds0)
+    i0 = ip0.inds[i]
+    iT = ipT.inds[i]
 
-        # else: point from nirvana to self; ignored 
+    if iT == 0 # (xT, yT) in nirvana
+        if i0 != 0 # (x0, y0) in domain
+            i0 = ip0.contains[i0] # index of cleaned polygon
+            P_closed[i0, n_polys + 1] = P_closed[i0, n_polys + 1] + 1
+        # elseif i0 == 0, (x0, y0) and (xT, yT) are both in nirvana; ignore
+        end
+    elseif iT in keys(ip0.contains) # (xT, yT) in the domain AND inside a box that contains data
+        iT = ip0.contains[iT] 
+        if i0 != 0 # (x0, y0) and (xT, yT) are both in domain
+            i0 = ip0.contains[i0] 
+            P_closed[i0, iT] = P_closed[i0, iT] + 1
+        elseif i0 == 0 # (x0, y0) in nirvana but (xT, yT) in domain (reinjection)
+            P_closed[n_polys + 1, iT] = P_closed[n_polys + 1, iT] + 1
+        end        
     end
 end
 
@@ -76,10 +53,10 @@ end
 Padj::Matrix{Int64} = [P_closed[i,j] != 0.0 ? 1 : 0 for i in 1:n_polys, j in 1:n_polys]
 
 # Construct the directed graph with adjacency matrix Padj and find its sccs; sort to get the largest scc.
-res = strongly_connected_components(SimpleDiGraph(Padj))
 scc = sort(strongly_connected_components(SimpleDiGraph(Padj)), by = length)[end]
 
 # In general, scc itself is not sorted, so we sort one my time and put nirvana back at the end to get the largest scc of P_closed
 largest_component = [sort(scc); n_polys + 1]
+P_closed = P_closed[largest_component, largest_component]
 
-P_sto = [P_closed[i, j]/sum(P_closed[i,:]) for i in 1:n_polys + 1, j in 1:n_polys+1];
+P_sto = [P_closed[i, j]/sum(P_closed[i,:]) for i in 1:n_polys, j in 1:n_polys];

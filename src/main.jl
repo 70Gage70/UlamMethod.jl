2f1f8f53238cc3e1e6590400a5c35e53eaae611a
Gage <gage.bonner@gmail.com>
2023-04-09 16:53:30 -0400

    ulam_write works

--- src/main.jl
+++ src/main.jl
@@ -22,24 +22,31 @@ function ulam_method(traj::UlamTrajectories, domain::UlamDomain)
     return ulam
 end
 
-# function ulam_method(fulam::String)::UlamResult
-#     traj = UlamTrajectories(fulam)
-#     domain = UlamDomain(fulam)
-#     return ulam_method(traj, domain)
-# end
+function ulam_write(outfile::String, ulam_result::UlamResult; P_out::Bool = true)
+    @assert outfile[end-2:end] == ".h5" "The output file must be of the form filename.h5"
 
-# need to write a function which writes an UlamResult to a file
+    fout = h5open(outfile, "w")
+    g = create_group(fout, "ulam")
 
-# if h5out
-#     # initialize output file 
-#     fout_name = "ulam_" * bin_type * "_" * string(n_polys) * extra_suffix * ".h5"
-#     rm(fout_name, force = true) # manually remove old file if it exists
-#     fout = h5open(fout_name, "w")
-#     write_dict_to_h5(fout, "ulam", ulam)
-#     close(fout)
+    g["n_polys"] = length(ulam_result.polys)
+    g["n_polys_dis"] = length(ulam_result.polys_dis)
+    g["pi_closed"] = ulam_result.pi_closed
 
-#     println("Output written to: " * fout_name)
-# end
+    # The polygons are output in to an n_polys x 3 matrix. The first two columns of the matrix are
+    # the (x, y) nodes and the third column is the index of the polygon that node belongs to.
+    g["polys"] = [PolyTable(ulam_result.polys).nodes ;; PolyTable(ulam_result.polys).edges[:,3]]
+
+    polys_dis_out = [PolyTable(ulam_result.polys_dis).nodes ;; PolyTable(ulam_result.polys_dis).edges[:,3]]
+    g["polys_dis"] = ulam_result.info.n_polys_dis == 0 ? zeros(0) : polys_dis_out
+
+    if P_out g["P_closed"] = ulam_result.P_closed end
+
+    close(fout)
+
+    @info "UlamResult written to $(outfile)."
+
+    return
+end
 
 
 
--- src/workspace.jl
+++ src/workspace.jl
@@ -5,8 +5,8 @@ include("main.jl")
 
 fin = "x0x5-NA-undrogued.mat"
 corners = [-100, 15, -9, 39]
-poly_number = 500
-poly_type = "hex"
+poly_number = 50
+poly_type = "vor"
 traj = UlamTrajectories(fin);
 domain = UlamDomain(corners..., poly_number = poly_number, poly_type = poly_type);
 ulam = ulam_method(traj, domain)

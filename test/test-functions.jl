7c8df1805bf281386f45cf982891a42b016e1193
Gage <gage.bonner@gmail.com>
2023-05-09 20:11:00 -0500

    test atol 1e-13

--- test/test-functions.jl
+++ test/test-functions.jl
@@ -1,10 +1,22 @@
+function test_eq(a::Any, b::Any)
+    if (length(a) == 0) && (length(b) == 0) && a == b
+        return true
+    elseif length(a) != length(b)
+        return false
+    else
+        return all(isapprox.(a, b, atol = 1e-13))
+    end
+end
+
 function ulam_test(ftest::String, ulam_result::UlamResult)
     testf = h5open(ftest)
 
     # pi_closed
     ulam = ulam_result.pi_closed
     test = read(testf["ulam/pi_closed"])
-    if !all(ulam .≈ test) 
+    if !test_eq(ulam, test)
+        @show ulam
+        @show test
         @error("pi_closed mismatch")
         return false 
     end 
@@ -12,7 +24,9 @@ function ulam_test(ftest::String, ulam_result::UlamResult)
     # P_closed
     ulam = ulam_result.P_closed
     test = read(testf["ulam/P_closed"])
-    if !all(ulam .≈ test) 
+    if !test_eq(ulam, test)
+        @show ulam
+        @show test
         @error("P_closed mismatch")
         return false 
     end 
@@ -20,7 +34,9 @@ function ulam_test(ftest::String, ulam_result::UlamResult)
     # polys
     ulam = PolyTable(ulam_result.polys).nodes
     test = read(testf["ulam/polys"])[:,1:2]
-    if !all(ulam .≈ test) 
+    if !test_eq(ulam, test)
+        @show ulam
+        @show test
         @error("polys mismatch") 
         return false 
     end    
@@ -31,7 +47,9 @@ function ulam_test(ftest::String, ulam_result::UlamResult)
     else
         ulam = PolyTable(ulam_result.polys_dis).nodes
         test = read(testf["ulam/polys_dis"])[:,1:2]
-        if !all(ulam .≈ test) 
+        if !test_eq(ulam, test)
+            @show ulam
+            @show test
             @error("polys_dis mismatch")
             return false
         end

5c3efbc91ed133c707e0952ddb032dc24dfac13b
Gage <gage.bonner@gmail.com>
2024-06-28 23:20:38 -0400

    boundary box

--- meshes/boundary.jl
+++ meshes/boundary.jl
@@ -9,10 +9,22 @@ The boundary is partitioned according to an [`BinningAlgorithm`](@ref).
 
 - `boundary`: A `Polytope` defining the boundary.
 
-### Constructor
+### 1D Constructor
 
-- 1D: `Boundary(boundary::Segment)`
-- 2D: `Boundary(boundary::Ngon)`
+In 1D, the boundary is a continuous line segment between `x_start` and `x_end`. Use
+
+`Boundary(x_start, x_end)`
+
+### 2D Constructors
+
+In 2D, the boundary is a closed polygon with vertices `verts`, which should be be a
+vector of `[x, y]` coordinates. Use
+
+`Boundary(verts)`
+
+A convenience constructor is also provided for the case of a rectangular boundary. Use
+
+`Boundary(xmin, xmax, ymin, ymax)`
 """
 struct Boundary{K, Dim, CRS}
     boundary::Polytope{K, Dim, CRS}
@@ -23,7 +35,22 @@ struct Boundary{K, Dim, CRS}
 end
 
 ### 1D
-Boundary(boundary::Segment) = Boundary(boundary = boundary)
+function Boundary(x_start::Real, x_end::Real)
+    @argcheck x_start < x_end
+
+    return Boundary(boundary = Segment(Meshes.Point(float(x_start)), Meshes.Point(float(x_end))))
+end
 
 ### 2D
-Boundary(boundary::Ngon) = Boundary(boundary = boundary)
+function Boundary(verts::Vector{<:Tuple{Real, Real}})
+    @argcheck all(length.(verts) .== 2)
+
+    return Boundary(boundary = Ngon(verts...))
+end
+
+function Boundary(xmin::Real, xmax::Real, ymin::Real, ymax::Real)
+    @argcheck xmin < xmax
+    @argcheck ymin < ymax
+
+    return Boundary([(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)])
+end
--- meshes/test.jl
+++ meshes/test.jl
@@ -1,7 +1,7 @@
 using Meshes, CairoMakie
 using ArgCheck
 using PolygonInbounds
-using Graphs:SimpleDiGraph, strongly_connected_components
+using Graphs: SimpleDiGraph, strongly_connected_components
 
 using Distributions # FOR TESTING ONLY
 
@@ -15,7 +15,7 @@ include("main.jl")
 
 
 ### 1d
-boundary1d = Segment((0,), (1,)) |> Boundary
+boundary1d = Boundary(0, 1)
 traj1d = Trajectories(rand(1, 10), rand(1, 10))
 
 ur = ulam_method(traj1d, boundary1d, LineBinner(500))
@@ -25,6 +25,7 @@ x0_rand = rand(MvNormal([1, 4], [1 0; 0 1]), 10000)
 xT_rand = x0_rand + rand(MvNormal([0, 0], [1 0; 0 1]), size(x0_rand, 2))
 traj2d = Trajectories(x0_rand, xT_rand)
 
-boundary2d = Ngon([(0,0),(6,0),(1,7),(1,6)]...) |> Boundary
+boundary2d = Boundary([(0,0),(6,0),(1,7),(1,6)])
+boundary2d = Boundary(0, 6, 0, 4)
 
 ur = ulam_method(traj2d, boundary2d, RectangleBinner(500))

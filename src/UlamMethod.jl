805e7da7b0956b305dcadfd6c974404c602d78b8
Gage <gage.bonner@gmail.com>
2024-09-20 19:11:50 -0500

    app

--- src/UlamMethod.jl
+++ src/UlamMethod.jl
@@ -105,8 +105,87 @@ include("earth-polygons.jl") # EarthPolygons module
     ur = ulam_method(traj3d, HyperRectangleBinner(1000, boundary3d), reinj_algo = SourceReinjection([(1, 2, 3)])) 
 end
 
+using MAT
+
+function _ulam(ARGS_bin_type::String)
+  ######### EDITING SECTION #########
+  ###################################
+
+  # path to the mat file with data and variable names
+  file_in = "/Users/gagebonner/Desktop/Repositories/SparseSargassum.jl/data/x0x5-2018-water.mat"
+  x0_name, xT_name, y0_name, yT_name = "x0", "xT", "y0", "yT"
+
+  bin_type = ARGS_bin_type    # bin type, should be one of "rec", "tri", "hex" or "vor"
+  n_bins = 500        # number of bins requested
+
+  # The fraction of data to place in nirvana on each side of the computational domain
+  # e.g. if ω_bot = 0.02, then the box will be drawn such that 2% of the data are below the box
+  ω_left  = 0.00
+  ω_right = 0.00
+  ω_bot   = 0.02
+  ω_top   = 0.02
+
+  # reinjection algorithm, should be one of "data", "stat" or "unif"
+  # "data": reinject according to actual trajectories
+  # "stat": reinject according to the stationary distribution
+  # "unif": reinject uniformly
+  reinj_type = "data"
+
+  # output file
+  # the output will be created here with the following 
+  # the output contains three fields, "P", "bins_verts" and "info"
+  # - P is P_closed
+  # - bins_verts is a list of vertices such that the ith entry in bins_verts corresponds to 
+  #   the ith row/column of P. The vertices are N_points x 2 matrices, e.g. they will be 
+  #   4 x 2 matrices for rectangular bins.
+  # - info is this information
+  file_out = "/Users/gagebonner/Desktop/Repositories/SparseSargassum.jl/data/ulam-out.mat"
+
+
+
+
+  ########## EDIT PAST HERE AT YOUR OWN RISK :) ##########
+  ########################################################
+  ########################################################
+
+  x0, xT, y0, yT = matread(file_in) |> f -> (f[x0_name], f[xT_name], f[y0_name], f[yT_name]) .|> vec
+  traj = Trajectories(permutedims(hcat(x0, y0)), permutedims(hcat(xT, yT)))
+  boundary = AutoBoundary(traj, nirvana = [(ω_left, ω_right), (ω_bot, ω_top)])
+
+  if bin_type == "rec"
+      binner = RectangleBinner(n_bins, boundary, hardclip = false)
+  elseif bin_type == "tri"
+      binner = TriangleBinner(n_bins, boundary, hardclip = false)
+  elseif bin_type == "hex"
+      binner = HexagonBinner(n_bins, boundary, hardclip = false)
+  elseif bin_type == "vor"
+      binner = VoronoiBinner(n_bins, boundary, traj, hardclip = false)
+  else
+      error("Unsupported bin type.")
+  end
+
+  if reinj_type == "data"
+      reinj_algo = DataReinjection()
+  elseif reinj_type == "stat"  
+      reinj_algo = StationaryReinjection()
+  elseif reinj_type == "unif"  
+      reinj_algo = UniformReinjection()
+  end
+
+  ulam = ulam_method(traj, binner, reinj_algo = reinj_algo)
+  P = P_closed(ulam)
+  bins_verts = points(bins(ulam))
+  bins_verts = [b .|> collect |> x -> stack(x, dims = 1) for b in bins_verts]
+  info = "P is P_closed. bins_verts is a list of vertices such that the ith entry in bins_verts \
+  corresponds to the ith row/column of P. The vertices are N_points x 2 matrices, e.g. they will be \
+  4 x 2 matrices for rectangular bins."
+  matwrite(file_out, Dict("P" => P, "bins_verts" => bins_verts, "info" => info))
+
+  return nothing
+end
+
 function julia_main()::Cint
-    # do something based on ARGS?
+    _ulam(ARGS[1])
     return 0 # if things finished successfully
   end
 
